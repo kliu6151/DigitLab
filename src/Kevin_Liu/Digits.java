56793f28af687d58138e65f351e1172ed3297326
BT_1N3_29@bktech.local <BT_1N3_29@bktech.local>
2018-12-11 11:28:46 -0500

    Done

---
+++ src/Kevin_Liu/Digits.java
@@ -0,0 +1,40 @@
+package Kevin_Liu;
+
+import java.util.ArrayList;
+
+public class Digits {
+    private ArrayList<Integer> digitList;
+
+    public Digits(int num)
+    {
+        digitList = new ArrayList<Integer>();
+        if(num == 0) {
+            digitList.add(new Integer(0));
+        }
+            while(num > 0)
+            {
+                digitList.add(0, new Integer(num % 10));
+                num /= 10;
+            }
+    }
+    public boolean isStrictlyIncreasing()
+    {
+        for(int i = 0;i<digitList.size()-1;i++)
+        {
+            if(digitList.get(i).intValue() >= digitList.get(i+1).intValue())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public int[] getDigitList()
+    {
+        int arr[] = new int[digitList.size()];
+        for(int i = 0;i<digitList.size()-1;i++)
+        {
+            arr[i]= digitList.get(i);
+        }
+        return arr;
+    }
+}
